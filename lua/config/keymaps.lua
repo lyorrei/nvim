73b8735403563c325600193ea03e94b2e7f92eb5
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2024-06-02 01:01:51 -0300

    Autocomplete with tab, telescope mapping and beginning/end of line mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- exit insert mode with jk
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+
+-- Perusing code faster with K and J
+-- vim.keymap.set({ "n", "v" }, "K", "5k", { noremap = true, desc = "Up faster" })
+-- vim.keymap.set({ "n", "v" }, "J", "5j", { noremap = true, desc = "Down faster" })
+
+-- Remap K and J
+-- vim.keymap.set({ "n", "v" }, "<leader>k", "K", { noremap = true, desc = "Keyword" })
+-- vim.keymap.set({ "n", "v" }, "<leader>j", "J", { noremap = true, desc = "Join lines" })
+
+vim.keymap.set("n", "H", "^", { noremap = true, silent = true, desc = "Beginning of line" })
+vim.keymap.set("n", "L", "$", { noremap = true, silent = true, desc = "End of line" })
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    -- install fzf native
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    keys = {
+      -- change a keymap
+      { "<C-p>", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+      -- add a keymap to browse plugin files
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+  },
+}
