c9db0940226b7a377b32b13b29220b164065c728
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2024-06-02 23:30:54 -0300

    Fix visual multi mappings

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -2,5 +2,14 @@ return {
   {
     "mg979/vim-visual-multi",
     branch = "master",
+    init = function()
+      vim.g.VM_mouse_mappings = 1
+      vim.g.VM_default_mappings = 0
+      vim.g.VM_maps = {
+        ["Select Cursor Down"] = "<C-M-Down>",
+        ["Select Cursor Up"] = "<C-M-Up>",
+        ["Mouse Cursor"] = "<M-LeftMouse>",
+      }
+    end,
   },
 }
