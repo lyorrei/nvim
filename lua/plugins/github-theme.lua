b8443532e6530cefe8b82e7c1f9ba0b182b62d5c
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2024-06-05 10:34:39 -0300

    Changed venv selector and default theme

--- lua/plugins/github-theme.lua
+++ lua/plugins/github-theme.lua
@@ -11,7 +11,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark",
+      colorscheme = "github_dark_default",
     },
   },
 }
--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -2,14 +2,22 @@ return {
   {
     "mg979/vim-visual-multi",
     branch = "master",
+    lazy = false,
     init = function()
-      vim.g.VM_mouse_mappings = 1
       vim.g.VM_default_mappings = 0
+      vim.g.VM_mouse_mappings = 1
+
       vim.g.VM_maps = {
         ["Select Cursor Down"] = "<C-M-Down>",
         ["Select Cursor Up"] = "<C-M-Up>",
-        ["Mouse Cursor"] = "<M-LeftMouse>",
       }
+      -- Set basic mouse mappings for vim-visual-multi
+      vim.g.VM_mouse_mappings = 1
+
+      -- Custom mouse mappings
+      vim.api.nvim_set_keymap("n", "<M-LeftMouse>", "<Plug>(VM-Mouse-Cursor)", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<M-RightMouse>", "<Plug>(VM-Mouse-Word)", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<M-C-RightMouse>", "<Plug>(VM-Mouse-Column)", { noremap = true, silent = true })
     end,
   },
 }
